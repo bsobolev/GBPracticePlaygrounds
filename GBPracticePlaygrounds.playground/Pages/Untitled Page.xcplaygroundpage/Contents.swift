6c64b414b6180223ed766a355f1ff99ad1ab31cb
Boris Sobolev <rafinad@outlook.com>
2021-07-05 22:15:22 +0300

    lesson 2 webinar

--- GBPracticePlaygrounds.playground/Pages/Untitled Page.xcplaygroundpage/Contents.swift
+++ GBPracticePlaygrounds.playground/Pages/Untitled Page.xcplaygroundpage/Contents.swift
@@ -1,1 +1,23 @@
 import UIKit
+
+true && true
+true && false
+false && true
+false && false
+
+
+
+
+true || true
+true || false
+false || true
+false || false
+
+!true
+!false
+
+
+let r = 5
+let t = 7
+(r == 5) && (t > 8)
+
